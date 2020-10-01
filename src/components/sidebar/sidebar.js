2faa156f3ce91f58d6508e2fe468a33d4fbb12f4
Kyle Caprio <capriok7@yahoo.com>
2020-09-30 20:24:17 -0700

    profile init

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -141,7 +141,13 @@ const SidebarContent = ({ setSidebar }) => {
 }
 
 const SidebarOptions = ({ flipOptions }) => {
-	const [, dispatch] = useStateValue()
+	const [{
+		user: {
+			details: {
+				uid
+			}
+		},
+	}, dispatch] = useStateValue()
 
 	const logoutActions = async () => {
 		await dispatch({ type: 'LOGOUT' })
@@ -154,7 +160,7 @@ const SidebarOptions = ({ flipOptions }) => {
 		<div className={ops.options}>
 			<h1 className={ops.profile}>Profile</h1>
 			<ul>
-				<Link to="/user"><li>View Profile</li></Link>
+				<Link to={`/user/profile`}><li>View Profile</li></Link>
 			</ul>
 			<h1>Options</h1>
 			<ul>
--- src/views/profile.js
+++ src/views/profile.js
@@ -1,30 +1,35 @@
 /*eslint react-hooks/exhaustive-deps: "off"*/
 /*eslint no-unused-vars: "off"*/
-import React, { useState, useEffect } from 'react'
-import { SQLiftingAcc } from '../api/sqlifting'
+import React, { useState, useEffect, useRef } from 'react'
 import { useStateValue } from '../state/state'
+import { SQLiftingAcc } from '../api/sqlifting'
+
+import { Input } from 'godspeed'
+
 import styles from '../styles/profile/profile.module.scss'
+
 const Profile = () => {
 	const [{
 		user: {
 			details: {
 				uid
 			}
-		}
+		},
+		compositions,
+		composites
 	},] = useStateValue()
 
 	const [profile, setProfile] = useState({})
+
+	const fileRef = useRef(null)
+	const inputRef = useRef('')
 	const [preview, setPreview] = useState()
-	const [icon, setIcon] = useState()
 
 	useEffect(() => {
 		SQLiftingAcc.get(`/profile/${uid}`)
 			.then(res => {
-				const result = {
-					...res.data,
-					icon: 'data:image/png;base64,' + new Buffer(`${profile.icon}`, 'binary').toString('base64')
-				}
-				setProfile(result)
+				console.log(res);
+				setProfile(res.data)
 			})
 			.catch(err => console.log(err))
 	}, [])
@@ -33,24 +38,26 @@ const Profile = () => {
 		console.log(profile);
 	}, [profile])
 
-
 	const changeIcon = (e) => {
 		let reader = new FileReader();
 		const file = e.target.files[0];
+		console.log(file);
 		if (file) {
-			console.log("new pic", file);
 			reader.onloadend = () => {
-				setPreview(reader.result);
+				setPreview(reader.result)
 			};
-			setIcon(file);
-			reader.readAsDataURL(file);
+			fileRef.current = file
+			reader.readAsDataURL(file)
 		}
 	}
 
-	const updateIcon = () => {
+	const submitIcon = (e) => {
+		e.preventDefault();
 		let formData = new FormData();
+		const file = fileRef.current
+		if (!file) return
 		formData.append('uid', uid);
-		formData.append('icon', icon);
+		formData.append('icon', fileRef.current);
 		SQLiftingAcc.post('/updateIcon', formData,
 			{
 				withCredentials: true,
@@ -59,25 +66,92 @@ const Profile = () => {
 				}
 			})
 			.then(res => {
-				console.log(res);
+				setProfile({
+					...profile,
+					icon: res.data.data
+				})
+				setPreview();
+				inputRef.current.value = ''
+				fileRef.current.value = 'null'
 			})
 			.catch(err => console.log(err))
 	}
 
 	return (
 		<div className={styles.profile}>
-			<h1>Profile</h1>
-			<p>{profile.username}</p>
-			<input
-				accept="image/jpeg, image/png"
-				type="file"
-				onChange={e => changeIcon(e)} />
-			<button onClick={() => updateIcon()}>Submit</button>
-			<img src={preview} alt=""></img>
-			<p>{profile.join_date}</p>
+			<nav>
+				<div>
+					<h1>{profile.username}</h1>
+					<img src={profile.icon} alt="" />
+				</div>
+				<div>
+					<p>Followers<span>{profile.followers}</span></p>
+					<p>Following<span>{profile.following}</span></p>
+					<button>Follow</button>
+				</div>
+			</nav>
+			<main>
+				{/* <section className={styles.details}>
+					<h1>Details</h1>
+					<p>First Name <Input type="text" value={profile.first_name} /></p>
+					<p>Last Name <Input type="text" value={profile.last_name} /></p>
+					<p>Biography <Input type="text" value={profile.bio} /></p>
+				</section> */}
+				<section className={styles.compositions}>
+					<div>
+						<p>Equipmnents</p>
+						<p>{compositions.equipments.length}</p>
+					</div>
+					<div>
+						<p>Muscles</p>
+						<p>{compositions.muscles.length}</p>
+					</div>
+					<div>
+						<p>Exercises</p>
+						<p>{compositions.exercises.length}</p>
+					</div>
+					<div>
+						<p>Movements</p>
+						<p>{compositions.movements.length}</p>
+					</div>
+				</section>
+				<section className={styles.composites}>
+					<div>
+						<p>Exercises</p>
+						<p>{composites.excos.length}</p>
+					</div>
+					<div>
+						<p>Circuits</p>
+						<p>{composites.circs.length}</p>
+					</div>
+					<div>
+						<p>Workouts</p>
+						<p>{composites.wocos.length}</p>
+					</div>
+				</section>
+			</main>
+
+
+			{/* <h1>Profile</h1>
+			<p>Usernmae: {profile.username}</p>
 			<div>
-				<img style={{ maxWidth: '100px' }} src={profile.icon} alt=""></img>
+				<p>Profile Icon</p>
+				<img src={profile.icon} alt=""></img>
 			</div>
+			<p>Join Date: {profile.join_date}</p>
+			<div>
+				<p>Update icon</p>
+				<form onSubmit={e => submitIcon(e)}>
+					<input
+						ref={inputRef}
+						accept="image/jpeg, image/png"
+						type="file"
+						onChange={e => changeIcon(e)} />
+					<button type="submit">Submit</button>
+				</form>
+				<p>Preview</p>
+				<img src={preview} alt="" />
+			</div> */}
 		</div>
 	)
 }
