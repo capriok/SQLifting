42ab770645590ae8259b247cf58dc047366be9ed
Kyle Caprio <capriok7@yahoo.com>
2020-10-01 13:41:08 -0700

    profile: markup and styling, edit logic

--- src/views/profile.js
+++ src/views/profile.js
@@ -4,9 +4,12 @@ import React, { useState, useEffect, useRef } from 'react'
 import { useStateValue } from '../state/state'
 import { SQLiftingAcc } from '../api/sqlifting'
 
-import { Input } from 'godspeed'
+import { Button, Input } from 'godspeed'
+
+import nullIcon from '../images/null-icon.png'
 
 import styles from '../styles/profile/profile.module.scss'
+
 
 const Profile = () => {
 	const [{
@@ -19,19 +22,24 @@ const Profile = () => {
 		composites
 	},] = useStateValue()
 
-	const [profile, setProfile] = useState({})
-
 	const fileRef = useRef(null)
 	const inputRef = useRef('')
-	const [preview, setPreview] = useState()
+	const [editing, setEdit] = useState(false)
+	const [profile, setProfile] = useState({})
 
-	useEffect(() => {
+	const [changes, setChanges] = useState({})
+
+	const fetchProfile = () => {
 		SQLiftingAcc.get(`/profile/${uid}`)
 			.then(res => {
 				console.log(res);
 				setProfile(res.data)
 			})
 			.catch(err => console.log(err))
+	}
+
+	useEffect(() => {
+		fetchProfile()
 	}, [])
 
 	useEffect(() => {
@@ -44,21 +52,31 @@ const Profile = () => {
 		console.log(file);
 		if (file) {
 			reader.onloadend = () => {
-				setPreview(reader.result)
+				setChanges({
+					...changes,
+					icon: reader.result
+				})
 			};
 			fileRef.current = file
 			reader.readAsDataURL(file)
 		}
 	}
 
-	const submitIcon = (e) => {
-		e.preventDefault();
+	const cancelChanges = () => {
+		setEdit(false)
+		setChanges({})
+	}
+
+	const saveChanges = () => {
 		let formData = new FormData();
-		const file = fileRef.current
-		if (!file) return
 		formData.append('uid', uid);
 		formData.append('icon', fileRef.current);
-		SQLiftingAcc.post('/updateIcon', formData,
+		formData.append('first_name', changes.first_name);
+		formData.append('last_name', changes.last_name);
+		formData.append('email', changes.email);
+		formData.append('birthday', changes.birthday);
+		formData.append('location', changes.location);
+		SQLiftingAcc.post('/updateProfile', formData,
 			{
 				withCredentials: true,
 				headers: {
@@ -66,92 +84,136 @@ const Profile = () => {
 				}
 			})
 			.then(res => {
-				setProfile({
-					...profile,
-					icon: res.data.data
-				})
-				setPreview();
-				inputRef.current.value = ''
+				setEdit(false)
+				setChanges({})
+				fetchProfile()
 				fileRef.current.value = 'null'
 			})
 			.catch(err => console.log(err))
 	}
+
 
 	return (
 		<div className={styles.profile}>
 			<nav>
 				<div>
 					<h1>{profile.username}</h1>
-					<img src={profile.icon} alt="" />
+					<div className={styles.icon}>
+						{editing
+							? <img
+								src={changes.icon !== undefined ? changes.icon : profile.icon}
+								alt="" />
+							: <img
+								src={profile.icon !== null ? profile.icon : nullIcon}
+								alt="" />
+						}
+					</div>
+					{editing && <>
+						<label htmlFor="iconInput">Change Icon</label>
+						<input
+							id="iconInput"
+							ref={inputRef}
+							accept="image/jpeg, image/png"
+							type="file"
+							onChange={e => changeIcon(e)} />
+					</>}
 				</div>
 				<div>
 					<p>Followers<span>{profile.followers}</span></p>
 					<p>Following<span>{profile.following}</span></p>
-					<button>Follow</button>
+					<Button
+						text="Edit Profile"
+						size="xsm"
+						onClick={() => setEdit(true)}
+						disabled={editing} />
 				</div>
 			</nav>
 			<main>
-				{/* <section className={styles.details}>
-					<h1>Details</h1>
-					<p>First Name <Input type="text" value={profile.first_name} /></p>
-					<p>Last Name <Input type="text" value={profile.last_name} /></p>
-					<p>Biography <Input type="text" value={profile.bio} /></p>
-				</section> */}
-				<section className={styles.compositions}>
-					<div>
-						<p>Equipmnents</p>
-						<p>{compositions.equipments.length}</p>
+
+				<div className={styles.head_title}>
+					<h1>Profile</h1>
+					<p>Join Date | {profile.join_date}</p>
+				</div>
+				<section className={styles.head}>
+					<div className={styles.stats}>
+						<ul>
+							<h2>Compositions</h2>
+							<li><p>Equipmnents</p><span>{compositions.equipments.length}</span></li>
+							<li><p>Muscles</p><span>{compositions.muscles.length}</span></li>
+							<li><p>Exercises</p><span>{compositions.exercises.length}</span></li>
+							<li><p>Movements</p><span>{compositions.movements.length}</span></li>
+							<h2>Composites</h2>
+							<li><p>Exercises</p><span>{composites.circs.length}</span></li>
+							<li><p>Circuits</p><span>{composites.excos.length}</span></li>
+							<li><p>Workouts</p><span>{composites.wocos.length}</span></li>
+						</ul>
 					</div>
-					<div>
-						<p>Muscles</p>
-						<p>{compositions.muscles.length}</p>
-					</div>
-					<div>
-						<p>Exercises</p>
-						<p>{compositions.exercises.length}</p>
-					</div>
-					<div>
-						<p>Movements</p>
-						<p>{compositions.movements.length}</p>
-					</div>
-				</section>
-				<section className={styles.composites}>
-					<div>
-						<p>Exercises</p>
-						<p>{composites.excos.length}</p>
-					</div>
-					<div>
-						<p>Circuits</p>
-						<p>{composites.circs.length}</p>
-					</div>
-					<div>
-						<p>Workouts</p>
-						<p>{composites.wocos.length}</p>
+					<div className={styles.account}>
+						<ul>
+							<h2>Account</h2>
+							<li>
+								<p>First name</p>
+								{editing
+									? <Input
+										placeholder={profile.first_name}
+										onChange={e => setChanges({ ...changes, first_name: e.target.value })} />
+									: <span>{profile.first_name}</span>
+								}
+							</li>
+							<li>
+								<p>Last name</p>
+								{editing
+									? <Input
+										placeholder={profile.last_name}
+										onChange={e => setChanges({ ...changes, last_name: e.target.value })} />
+									: <span>{profile.last_name}</span>
+								}
+							</li>
+							<li>
+								<p>Email</p>
+								{editing
+									? <Input
+										placeholder={profile.email}
+										onChange={e => setChanges({ ...changes, email: e.target.value })} />
+									: <span>{profile.email}</span>
+								}
+							</li>
+							<li>
+								<p>Birthday</p>
+								{editing
+									? <Input
+										placeholder={profile.birthday}
+										onChange={e => setChanges({ ...changes, birthday: e.target.value })} />
+									: <span>{profile.birthday}</span>
+								}
+							</li>
+							<li>
+								<p>Location</p>
+								{editing
+									? <Input
+										placeholder={profile.location}
+										onChange={e => setChanges({ ...changes, location: e.target.value })} />
+									: <span>{profile.location}</span>
+								}
+							</li>
+						</ul>
 					</div>
 				</section>
 			</main>
-
-
-			{/* <h1>Profile</h1>
-			<p>Usernmae: {profile.username}</p>
-			<div>
-				<p>Profile Icon</p>
-				<img src={profile.icon} alt=""></img>
-			</div>
-			<p>Join Date: {profile.join_date}</p>
-			<div>
-				<p>Update icon</p>
-				<form onSubmit={e => submitIcon(e)}>
-					<input
-						ref={inputRef}
-						accept="image/jpeg, image/png"
-						type="file"
-						onChange={e => changeIcon(e)} />
-					<button type="submit">Submit</button>
-				</form>
-				<p>Preview</p>
-				<img src={preview} alt="" />
-			</div> */}
+			{editing &&
+				<footer>
+					<div>
+						<Button
+							text="Cancel"
+							size="xsm"
+							onClick={() => cancelChanges()} />
+						<Button
+							text="Save Changes"
+							size="xsm"
+							onClick={() => saveChanges()} />
+					</div>
+				</footer>
+			}
 		</div>
 	)
 }
